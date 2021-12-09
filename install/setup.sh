88eddd1a1fead4a609114c56937470b19325c39a
VanillaBrooks <VanillaBrooks@users.noreply.github.com>
2021-12-09 01:06:23 +0000

    deploy: Fluid-Dynamics-Group/distribute@1b229c083a21d67c843641887cb8186e6161f77b

--- install/setup.sh
+++
@@ -1,2 +0,0 @@
-mkdir /home/distribute/data
-mkdir /home/distribute/logs
--- install/update.sh
+++
@@ -1,5 +0,0 @@
-# update the `distribute` binary
-# this script must be run from within the `distribute` 
-# repository
-git pull && \
-cargo install --path .
